7dea629f701b61b67b27cec0d3e408e3c94d63a8
Taras Noga <taraskin2012@gmail.com>
2023-08-22 13:48:58 +0300

    add mastering-javascript page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,9 +1,12 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
+import { language } from "gray-matter";
 
 function PostContent(props) {
   const { post } = props;
@@ -39,6 +42,11 @@ function PostContent(props) {
       }
       return <p>{paragraph.children} </p>;
     },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return <SyntaxHighlighter style={atomDark} language={language} children={children}/>
+    }
   };
 
   return (
