c5dff1846caa024f81ac788cc1fe5749e7555dd2
Taras Noga <taraskin2012@gmail.com>
2023-08-24 19:26:16 +0300

    add Head to all pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
+
 import ContactForm from "../components/contact/contact-form";
 
 function ContactPage() {
-   return <ContactForm/>
+  return (
+    <>
+        <Head>
+           <title>Contact me</title>
+           <meta name="description" content="Send me your message"/>
+      </Head>
+      <ContactForm />
+    </>
+  );
 }
 
 export default ContactPage;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../helpers/post-util";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Taras' blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
