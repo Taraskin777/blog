04e7d7c77c9037e0cca4e39a935de5d3619e3131
Taras Noga <taraskin2012@gmail.com>
2023-11-18 15:30:08 +0200

    show env

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-// const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-// module.exports = (phase) => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER) {
-//     return {
-//       env: {
-//         mongodb_username: "taraskin",
-//         mongodb_password: "bRt4Zlx1ydkkdqVK",
-//         mongodb_clustername: "cluster0",
-//         mongodb_database: "my-blog-dev",
-//       },
-//     };
-//   }
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "taraskin",
+        mongodb_password: "bRt4Zlx1ydkkdqVK",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-blog-dev",
+      },
+    };
+  }
 
-//   return {
-//     env: {
-//       mongodb_username: "taraskin",
-//       mongodb_password: "bRt4Zlx1ydkkdqVK",
-//       mongodb_clustername: "cluster0",
-//       mongodb_database: "my-blog",
-//     },
-//   };
-// };
+  return {
+    env: {
+      mongodb_username: "taraskin",
+      mongodb_password: "bRt4Zlx1ydkkdqVK",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-blog",
+    },
+  };
+};
